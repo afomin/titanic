7d053dcbaaa2a4a1bb99eb63fd89cffebdbf7564
afomin <fomin.alexey@gmail.com>
2017-11-18 00:52:48 +0300

    initial commit

---
+++ src/main/java/com/afomin/test/teamcity/Titanic.java
@@ -0,0 +1,22 @@
+package com.afomin.test.teamcity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Titanic {
+    public Titanic() {
+    }
+
+    public String getVersion() throws IOException {
+        Properties properties = new Properties();
+
+
+        InputStream propertiesStream = getClass().getClassLoader().getResourceAsStream("version.properties");
+        if (propertiesStream != null) {
+            properties.load(propertiesStream);
+            return properties.getProperty("version");
+        }
+        return null;
+    }
+}
